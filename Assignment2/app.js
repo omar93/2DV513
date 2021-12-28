9cc69d92288440d00d5bcad6046a714aea2f1c50
Omar Alhamad <omar93@hotmail.se>
2021-12-28 21:38:57 +0100

    can now read all the lines from the RC_2007-10 file, now I need to insert the data to the db

--- Assignment2/app.js
+++ Assignment2/app.js
@@ -1,1 +1,13 @@
 const mysql = require('mysql')
+const fs = require('fs')
+const readline = require('readline')
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('./lib/RC_2007-10'),
+    output: process.stdout,
+    console: false
+});
+
+readInterface.on('line', function(line) {
+    console.log(line);
+});
