c08ecd76fdbc720c1cba485fcb1c79ee6d050184
Omar Alhamad <omar93@hotmail.se>
2021-12-30 22:02:26 +0100

    with and without constraints working

--- Assignment2/app.js
+++ Assignment2/app.js
@@ -1,13 +1,68 @@
 const mysql = require('mysql')
 const fs = require('fs')
 const readline = require('readline')
+const { performance } = require('perf_hooks')
 
-const readInterface = readline.createInterface({
-    input: fs.createReadStream('./lib/RC_2007-10'),
-    output: process.stdout,
-    console: false
-});
+let startTimer = performance.now()
 
-readInterface.on('line', function(line) {
-    console.log(line);
-});
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'reddit-keys'
+})
+
+
+connection.connect(function (error) {
+    if (!error) {
+        console.log('we are connected to the database')
+        readDataFromFile()
+    } else {
+        throw error
+    }
+})
+
+const readDataFromFile = () => {
+    
+    const lineReader = readline.createInterface({
+        input: fs.createReadStream('./lib/RC_2007-10')
+    })
+
+    lineReader.on('line', line => {
+        let data = JSON.parse(line)
+        saveToDB(data)
+    })
+
+}
+
+const saveToDB = data => {
+    let linkData = [[data.link_id.split('_')[1], data.link_id, data.subreddit]]
+    let linkTable = 'INSERT IGNORE INTO link (id, name, subreddit_id) VALUES ?'
+    connection.query(linkTable, [linkData],  (error, result) => {
+        if (error) {
+            throw error
+        }
+    })
+
+    let subredditData = [[data.subreddit_id, data.subreddit]]
+    let subredditTable = 'INSERT IGNORE INTO subreddit (id, name) VALUES ?'
+    connection.query(subredditTable, [subredditData], (error, result) => {
+        if (error) {
+            throw error
+        }
+    })
+
+    let commentData = [[data.id, data.name, data.author, data.score, data.body, data.created_utc, data.link_id, data.parent_id]]
+    let commentTable = 'INSERT IGNORE  INTO comments (id, name, author, score, body, created_utc, link_id, parent_id) VALUES ?'
+    connection.query(commentTable, [commentData], (error, result) => {
+        if (error) {
+            throw error
+        }
+        let endTimer = performance.now()
+        console.log(`Time without contraints ${endTimer - startTimer}ms`)
+        // without constraints it took 26 minutes or 1574891ms
+        // without constraints it took 13 minutes or 785052ms
+    })
+
+}
